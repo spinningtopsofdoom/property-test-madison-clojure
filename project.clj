075c2c1768311572360cc1a923580c76892113cf
Peter Schuck <peter@bendyworks.com>
2016-10-15 17:48:47 -0500

    Initial Clojure Setup

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject prop-clojure "1.0.0"
+  :description "Code for test.check talk for Madison Clojure"
+
+  :license  {:name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths  ["src"]
+
+  :dependencies  [[org.clojure/clojure "1.8.0"]
+                  [org.clojure/test.check "0.9.0"]])
