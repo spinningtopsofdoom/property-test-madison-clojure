b03fd29f2f15d25a430070be674ff3b85d3c85ce
Peter Schuck <peter@bendyworks.com>
2016-10-25 17:32:42 -0500

    Add ! to signify mutative functions

--- src/madison/test/friendship.clj
+++ src/madison/test/friendship.clj
@@ -26,13 +26,13 @@
 
 (def operations (gen/vector operation))
 
-(declare add-friendship remove-friendship)
+(declare add-friendship! remove-friendship!)
 (defn apply-operations [operations]
   (let [db (create-friendship-db)]
     (doseq [op operations]
       (case (first op)
-        :add-friendship (apply add-friendship (cons db (rest op)))
-        :remove-friendship (apply remove-friendship (cons db (rest op)))))
+        :add-friendship (apply add-friendship! (cons db (rest op)))
+        :remove-friendship (apply remove-friendship! (cons db (rest op)))))
     db))
 ; Properties
 (def no-self-friend
@@ -50,31 +50,31 @@
           (every? #(friends? db % person) (person-friends db person)))
         (get-people db)))))
 ; Command (stateful) Operaions
-(defn bad-add-friendship [db person new-friend]
+(defn bad-add-friendship! [db person new-friend]
   (let [friends (conj (person-friends db person) new-friend)]
     (swap! db #(assoc % person friends))
     db))
 
-(defn bad-remove-friendship [db person friend]
+(defn bad-remove-friendship! [db person friend]
   (let [friends (disj (person-friends db person) friend)]
     (swap! db #(assoc % person friends))
     db))
 
-(defn good-add-friendship [db person new-friend]
+(defn good-add-friendship! [db person new-friend]
   (when (not= person new-friend)
     (let [friends (conj (person-friends db person) new-friend)
           persons (conj (person-friends db new-friend) person)]
       (swap! db #(assoc % person friends new-friend persons))))
   db)
 
-(defn good-remove-friendship [db person friend]
+(defn good-remove-friendship! [db person friend]
   (let [friends (disj (person-friends db person) friend)
         persons (disj (person-friends db friend) person)]
     (swap! db #(assoc % person friends friend persons))
     db))
 
-(with-redefs [add-friendship good-add-friendship
-              remove-friendship good-remove-friendship]
+(with-redefs [add-friendship! good-add-friendship!
+              remove-friendship! good-remove-friendship!]
   (print "No Self Friending\n")
   (pprint/pprint (tc/quick-check 100 no-self-friend))
   (print "Symetrical Friendship\n")
